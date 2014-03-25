a2ead59e14851c6e04b370a312818ff5808e34fc
damiano.renfer <damiano.renfer@he-arc.ch>
2014-03-25 11:37:01 +0100

    Initial Classifier

---
+++ Classifier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#-*- coding : utf-8 -*-
+
+__author__ = 'Damiano Renfer & Mirco Nasuti'
+
+if __name__ == "__main__":
+
+    #TODO replace with mirco's loader
+    positive_texts = []
+    negative_texts = []
+    test_texts = []
+
+
+
