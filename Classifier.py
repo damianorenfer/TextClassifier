a2a2fcaea85e23405e71c9db48fc994368e6a211
damiano.renfer <damiano.renfer@he-arc.ch>
2014-04-22 11:31:55 +0200

    Added cross validation method

--- Classifier.py
+++ Classifier.py
@@ -44,7 +44,6 @@ def is_text_positive(text, probabilities_positive_word, probabilities_negative_w
         if k in probabilities_positive_word:
             positive_probability += probabilities_positive_word[k]**v
 
-
     #Negative probability
     for k, v in text.items():
         if k in probabilities_negative_word:
@@ -106,33 +105,7 @@ def select_knownledge_texts(positive_texts, negative_texts, knowledge_base_divis
 
     return knowledege_positive_list, knowledege_negative_list, positive_texts, negative_texts
 
-if __name__ == "__main__":
-
-    #Args parsing
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--tagged", help="Use tagged texts", action="store_true")
-    parser.add_argument("-d", "--division", help="Corpus division, percent of training texts", type=float)
-    args = parser.parse_args()
-
-    #Lists of dict : {word : count}
-    pathPosFiles = './data/pos/'
-    pathNegFiles = './data/neg/'
-    pathPosTaggedFiles = './data/tagged/pos/'
-    pathNegTaggedFiles = './data/tagged/neg/'
-    uselessWordsFileName = './data/frenchST.txt'
-
-    if not args.tagged:
-        positive_texts = countWords(pathPosFiles, uselessWordsFileName, False)
-        negative_texts = countWords(pathNegFiles, uselessWordsFileName, False)
-    else:
-        positive_texts = countWords(pathPosTaggedFiles, uselessWordsFileName, True)
-        negative_texts = countWords(pathNegTaggedFiles, uselessWordsFileName, True)
-
-    knowledge_base_division = KNOWLEDGE_BASE_DIVISION
-    if args.division:
-        knowledge_base_division = args.division
-    knowledge_positive_texts, knowledge_negative_texts, test_positive_texts, test_negative_texts = select_knownledge_texts(positive_texts, negative_texts, knowledge_base_division)
-
+def naive_validate(knowledge_positive_texts, knowledge_negative_texts, test_positive_texts, test_negative_texts) :
     #Calculate probabilities
     #Lists of dict : {word : probability}
     probabilities_positive_word = compute_probabilities(knowledge_positive_texts)
@@ -175,8 +148,103 @@ if __name__ == "__main__":
     print("Negative precision : %s" % negative_precision)
     print("Average precision : %s" % average_precision)
 
+
+def cross_validate(n, positive_texts, negative_texts):
+    """
+    N Folder - cross validation
+    """
+
+    folder_pos = []
+    folder_neg = []
+
+    offset_pos = int(len(positive_texts) / n)
+    offset_neg = int(len(negative_texts) / n)
+
+    for i in range(0, n):
+        folder_pos[i] = positive_texts[i*offset_pos:(i+1)*offset_pos]
+        folder_neg[i] = negative_texts[i*offset_neg:(i+1)*offset_neg]
+
+    for i in range(0, n):
+
+        knowledge_positive_texts = list(positive_texts - folder_pos[i])
+        knowledge_negative_texts = list(negative_texts - folder_neg[i])
+
+        #Calculate probabilities
+        #Lists of dict : {word : probability}
+        probabilities_positive_word = compute_probabilities(knowledge_positive_texts)
+        probabilities_negative_word = compute_probabilities(knowledge_negative_texts)
+
+        positive_texts_count = len(knowledge_positive_texts)
+        negative_texts_count = len(knowledge_negative_texts)
+        positive_apriori_probability = positive_texts_count / (positive_texts_count+negative_texts_count)
+
+        #Classificaiton
+        positive_match_count = 0
+        negative_match_count = 0
+
+        #Positive texts
+        positive_test_texts_count = len(test_positive_texts)
+        for j in range(0, positive_test_texts_count):
+            text = test_positive_texts[j]
+            is_positive = is_text_positive(text, probabilities_positive_word, probabilities_negative_word, positive_apriori_probability)
+            if is_positive:
+                positive_match_count += 1
+
+        #Negative texts
+        negative_test_texts_count = len(test_negative_texts)
+        for j in range(0, negative_test_texts_count):
+            text = test_negative_texts[j]
+            is_positive = is_text_positive(text, probabilities_positive_word, probabilities_negative_word, positive_apriori_probability)
+            if not is_positive:
+                negative_match_count += 1
+
+        positive_precision = positive_match_count/positive_test_texts_count
+        negative_precision = negative_match_count/negative_test_texts_count
+        average_precision = (positive_precision + negative_precision)/2
+
+        #Display results
+        print("Positive texts : %s" % len(test_positive_texts))
+        print("Positive texts matches : %s" % positive_match_count)
+        print("Positive precision : %s" % positive_precision)
+        print("Negative texts : %s" % len(test_negative_texts))
+        print("Negative texts matches : %s" % negative_match_count)
+        print("Negative precision : %s" % negative_precision)
+        print("Average precision : %s" % average_precision)
+
+
+if __name__ == "__main__":
+
+    #Args parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--tagged", help="Use tagged texts", action="store_true")
+    parser.add_argument("-d", "--division", help="Corpus division, percent of training texts", type=float)
+    args = parser.parse_args()
+
+    #Lists of dict : {word : count}
+    pathPosFiles = './data/pos/'
+    pathNegFiles = './data/neg/'
+    pathPosTaggedFiles = './data/tagged/pos/'
+    pathNegTaggedFiles = './data/tagged/neg/'
+    uselessWordsFileName = './data/frenchST.txt'
+
+    if not args.tagged:
+        positive_texts = countWords(pathPosFiles, uselessWordsFileName, False)
+        negative_texts = countWords(pathNegFiles, uselessWordsFileName, False)
+    else:
+        positive_texts = countWords(pathPosTaggedFiles, uselessWordsFileName, True)
+        negative_texts = countWords(pathNegTaggedFiles, uselessWordsFileName, True)
+
+    if args.division:
+        knowledge_base_division = args.division
+    else:
+        knowledge_base_division = KNOWLEDGE_BASE_DIVISION
+
+    knowledge_positive_texts, knowledge_negative_texts, test_positive_texts, test_negative_texts = select_knownledge_texts(list(positive_texts), list(negative_texts), knowledge_base_division)
+    #naive_validate(knowledge_positive_texts, knowledge_negative_texts, test_positive_texts, test_negative_texts)
+    cross_validate(5, positive_texts, negative_texts)
+
     #load probabilities from file
     #probabilities_positive_word, probabilities_negative_word, positive_apriori_probability = load_probabilities("knowledge_base-01-04-2014_10-59-53.json")
-    save_probabilities(probabilities_positive_word, probabilities_negative_word, positive_apriori_probability)
+    #save_probabilities(probabilities_positive_word, probabilities_negative_word, positive_apriori_probability)
 
 
